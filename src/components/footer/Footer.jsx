9f2ca88e29bd1b7abc4539bd1c1a4ec3cafb89db
akshay-k125 <akshay.raviraj@gmail.com>
2024-09-14 15:31:50 +0300

    Testimonial done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import "./footer.css";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="foooterContainer">
+        <div className="footerWrapper">
+          <div>
+            <p>click ir</p>
+            <p>summary</p>
+          </div>
+          <div>
+            <p>Our Services</p>
+            <p>Seo Marketing</p>
+            <p>Research Topic Trends</p>
+            <p>Email Marketing</p>
+            <p>Google PPC </p>
+          </div>
+          <div>
+            <p>Our Location</p>
+            <p>USA</p>
+            <p>UK</p>
+            <p>Canada</p>
+            <p>Virtual</p>
+          </div>
+          <div>
+            <p>Contact Us</p>
+            <p>email</p>
+            <p>phone</p>
+            <p>social media</p>
+          </div>
+          <div></div>
+        </div>
+        <hr/>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import "./testimonial.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
-  )
-}
+    <>
+      <div className="testimonialContainer">
+        <div className="testimonialMarginWrapper">
+          <div className="testimonialContentHeader">
+            <p>TESTIMONIAL</p>
+            <p>Customer is our top priority</p>
+            <p>
+              We servuey all of our clients, the results of which directly go to
+              our CEO
+            </p>
+          </div>
+          <div
+            className="testimonialContentContainer"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <button>
+              <FontAwesomeIcon icon={faArrowLeft} size="2x" />
+            </button>
+            <div className="testimonialContentCarasoul">
+              <div style={{ width: "30vw" }}>
+                <p>Gymstory</p>
+                <p>
+                  Kronix is the best digital Agency that I have Ever Seen!
+                  Highly Recommended{" "}
+                </p>
+                <p>
+                  I recently Hired Idepeol for a custome web development and
+                  couldn't be happier with the results. The team was able to
+                  bring my unique ideas to life and create a website that truely
+                  stands out.
+                </p>
+                <p>Diana Loreasia</p>
+                <p>Director of Gymstory</p>
+              </div>
+              <div>
+                <img
+                  style={{ width: "175px" }}
+                  src="https://avatars.githubusercontent.com/u/67403372?v=4"
+                  alt=""
+                />
+              </div>
+            </div>
+            <button>
+              <FontAwesomeIcon icon={faArrowRight} size="2x" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Testimonial
+export default Testimonial;
