4ad5daa85c4c33856741ac7d36552cfe4372cf69
akshay-k125 <akshay.raviraj@gmail.com>
2024-09-14 12:01:34 +0300

    service page done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import "./services.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch, faPenToSquare, faGlobe, faNewspaper, faEnvelope
+
+} from "@fortawesome/free-solid-svg-icons";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <>
+      <div className="serviceContainer">
+        <div className='marginWrapper'>
+          <div className="heading">Services</div>
+          <div className="serviceTag">
+            Strategic services drive digital success with tailored, comprehensive,
+            approaches
+          </div>
+          <div className="serviceMinionsContainer">
+            <div>
+              <FontAwesomeIcon icon={faSearch} size="5x" />
+              SEO Marketing
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faPenToSquare} size="5x" />
+              content creation
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faGlobe} size="5x" />
+              social media
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faNewspaper} size="5x" />
+              PPC
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faEnvelope} size="5x" />
+              Email Marketing
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Services
+export default Services;
